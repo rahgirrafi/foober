d9ea0d04fe63a59f7b43cbabd28b0f587eba29d5
rahgirrafi <112659971+rahgirrafi@users.noreply.github.com>
2022-11-08 22:23:40 +0600

    Update doomsday_fuel.py

--- doomsday_fuel.py
+++ doomsday_fuel.py
@@ -1,8 +1,7 @@
 def solution(s):
     from fractions import Fraction
-
+    from math import gcd
     def transpose_mat(a):
-        ###print(a)
         t = create_matrix(len(a[0]), len(a))
         for i in range(len(a)):
             for j in range(len(a[0])):
@@ -43,21 +42,44 @@ def solution(s):
         return I
 
     def minor_mat(m, i, j):
+        # # print(f'inside minor mat')
+        # # print(f'isde= {[row[:j] + row[j + 1:] for row in (m[:i] + m[i + 1:])]} ')
+        # # print(f'isde= {[row[:j] + row[j + 1:] for row in (m[:i] + m[i + 1:])]} ')
+        # # print(m)
+        # # print(f'going out')
         return [row[:j] + row[j + 1:] for row in (m[:i] + m[i + 1:])]
 
     def det_matrix(m):
         if len(m) == 2:
+            # # print(f'{m[0][0]} + {m[1][1]} - {m[0][1]} * {m[1][0]}')
+            # # print(f'returning {m[0][0] * m[1][1] - m[0][1] * m[1][0]}')
+
             return m[0][0] * m[1][1] - m[0][1] * m[1][0]
         determinant = 0
+        # # print('********************************')
         for c in range(len(m)):
-            determinant += ((-1) ** c) * m[0][c] * det_matrix(minor_mat(m, 0, c))
+            # # print(f'm = {m}')
+            # # print(f'{determinant} * (-1) ** {c} * {m[0][c]}')
+            # # print(f'before  calc {determinant}')
+            # # print(f'det {det_matrix(minor_mat(m, 0, c))}')
+            b = minor_mat(m, 0, c)
+            # # print(f'b = {b}')
+            d = det_matrix(b)
+            # # print(f'd = {d}')
+            # # print(f'd2= {((-1) ** c) * m[0][c]}')
+            determinant = determinant + ((-1) ** c) * m[0][c] * d
+            # # print(f'd3={determinant}')
+        # # print('returning')
         return determinant
 
     def matrix_inverse(m):
         determinant = det_matrix(m)
+        # # print(f'Fractioned determinent = {determinant}')
+        # # print(m)
         if len(m) == 2:
             return [[m[1][1] / determinant, -1 * m[0][1] / determinant],
                     [-1 * m[1][0] / determinant, m[0][0] / determinant]]
+        # # print(determinant)
         cofactors = []
         for r in range(len(m)):
             cofactorRow = []
@@ -68,7 +90,9 @@ def solution(s):
         cofactors = transpose_mat(cofactors)
         for r in range(len(cofactors)):
             for c in range(len(cofactors)):
+                # print(cofactors[r][c])
                 cofactors[r][c] = cofactors[r][c] / determinant
+
         return cofactors
 
     def get_Q(markov_chain):
@@ -115,8 +139,12 @@ def solution(s):
             s = [0] * len(matrix[0])
             for x in range(0, len(matrix[row])):
                 if sum(matrix[row]) != 0:
-                    s[x] = Fraction(matrix[row][x], sum(matrix[row]))
+                    # # print(matrix[row][x],sum(matrix[row]))
+                    g1 = Fraction(matrix[row][x])
+                    g2 = Fraction(sum(matrix[row]))
+                    s[x] = Fraction(g1 / g2)
             self.to_s = s
+
             route_to = []
 
             for x in s:
@@ -135,15 +163,25 @@ def solution(s):
     if len(s) == 1:
         return [1, 1]
     S = [state(s, i, i) for i in range(0, len(s))]
-    points = []
 
     terminal_state_amount = 0
     for x in S:
         if x.total_proable_change == 0:
             terminal_state_amount += 1
+    if terminal_state_amount == 1:
+        return [1, 1]
+
+    special_case=[]
+    if S[0].terminal == True:
+        special_case.append(0)
+        for i in range(0,terminal_state_amount-1):
+            special_case.append(0)
+        special_case.append(1)
+        return special_case
 
     conv_prob_matrix = create_matrix(len(s), len(s))
     conv_prob_matrix2 = create_matrix(len(s), len(s))
+
     for i in range(0, len(s)):
         if S[i].total_proable_change == 0:
             conv_prob_matrix[i][i] = 1
@@ -155,35 +193,44 @@ def solution(s):
 
     absorbing_points = []
     for i in range(0, len(conv_prob_matrix)):
+
         if 1 == conv_prob_matrix[i][i]:
             absorbing_points.append(i)
     absorbing_points.sort(reverse=True)
-
-    if len(s) < 4:
-        absorbing_points = set(absorbing_points)
-    anti_absorbe = []
-    for i in range(0, len(conv_prob_matrix)):
-        if i not in absorbing_points:
-            anti_absorbe.append(i)
-    anti_absorbe.sort(reverse=True)
-    count_terminal_state = 0
-    for x in range(0, len(s)):
-        if S[x].terminal == True:
-            count_terminal_state += 1
-            points.append(x)
 
     q = get_Q(conv_prob_matrix2)
     r = get_R(conv_prob_matrix)
     id_mat = identity_matrix(len(q))
     N = matrix_inverse(matrix_subtract(id_mat, q))
     probablity_matrix = matrix_multipy(N, r)
+    #print(probablity_matrix)
 
+    for i in range(0,len(probablity_matrix[0])):
+        probablity_matrix[0][i]= (probablity_matrix[0][i]).limit_denominator()
     dom = []
+
     for x in probablity_matrix[0]:
         dom.append(x.denominator)
-    max_dom = max(dom)
+
+    lcm = 1
+    for i in dom:
+        lcm = lcm * i // gcd(lcm, i)
+
     result = []
     for x in probablity_matrix[0]:
-        result.append(int(x * max_dom))
-    result.append(max_dom)
+        result.append(int(x * lcm))
+    result.append(lcm)
     return result
+
+
+s =[[0, 1, 3,  2, 1, 3, 0, 0],
+       [0, 0, 1, 0, 0, 0, 0, 0],
+       [0, 2, 0, 0, 0, 0, 3, 0],
+       [0, 0, 4, 0, 5, 0, 0, 7],
+       [0, 0, 0, 2, 0, 3, 0, 0],
+       [1, 4, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+print(solution(s))
